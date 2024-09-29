4b89634cda6b4b4bfb2cd579d148164f1a643d43
S Ray <selmen.rayen.92@gmail.com>
2024-09-29 12:15:52 +0100

    prepare body

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [userName, setUserName] = useState("");
+  const [user, setUser] = useState("");
+  console.log(userName);
+  console.log(user);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="login">
+        <input
+          type="text"
+          placeholder="username"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <button onClick={() => setUser(userName)}>Login</button>
+      </div>
     </div>
   );
 }
